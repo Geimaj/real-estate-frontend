d2cf7bcae931f50183651bb02248711b42cd40e7
geimaj <16000925@vcconnect.co.za>
2019-05-15 01:55:10 +0200

    refactor country

--- src/components/Country.vue
+++ src/components/Country.vue
@@ -1,37 +1,54 @@
 <template>
-  <v-container fluid>
-    <v-layout wrap>
-      <v-flex xs12>
-        <v-combobox
-          v-model="country"
-          :items="countries"
-          label="Countries"
-          item-text="name"
-          item-value="id"
-        ></v-combobox>
-      </v-flex>
+  <div>
+    <v-toolbar flat>
+      <v-toolbar-title>Countries</v-toolbar-title>
+      <v-divider class="mx-2" inset vertical></v-divider>
+      <v-spacer></v-spacer>
+      <v-dialog v-model="dialog" max-width="500px">
+        <template v-slot:activator="{ on }">
+          <v-btn color="primary" dark class="mb-2" v-on="on">New Country</v-btn>
+        </template>
+        <v-card>
+          <v-card-title>
+            <span class="headline">New Country</span>
+          </v-card-title>
 
-      <v-btn color="success" @click="addCountry">
-        Add
-      </v-btn>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md4>
+                  <v-text-field
+                    v-model="country.name"
+                    label="Country name"
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card-text>
 
-      <v-btn color="warning" v-if="country.id > 0" @click="editCountry">
-        EDIT
-      </v-btn>
-
-      <v-btn color="error" @click="deleteCountry">
-        DELETE
-      </v-btn>
-
-      <CountryDialog
-        v-if="dialog"
-        :country="country"
-        :onSave="save"
-        :onCancel="cancel"
-      >
-      </CountryDialog>
-    </v-layout>
-  </v-container>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-toolbar>
+    <v-data-table :headers="headers" :items="countries" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.name }}</td>
+        <td class="justify-center layout px-0">
+          <v-icon small class="mr-2" @click="editCountry(props.item)">
+            edit
+          </v-icon>
+          <v-icon small @click="deleteCountry(props.item)">
+            delete
+          </v-icon>
+        </td>
+      </template>
+    </v-data-table>
+  </div>
 </template>
 
 <script>
@@ -45,11 +62,20 @@ export default {
   data() {
     return {
       dialog: false,
+      headers: [
+        {
+          text: 'Name',
+          align: 'left',
+          value: 'name',
+        },
+        {
+          text: 'Actions',
+          align: 'left',
+          sortable: false,
+        },
+      ],
       countries: [],
-      country: {
-        id: -1,
-        name: '',
-      },
+      country: { id: -1, name: '' },
     };
   },
   mounted() {
@@ -61,34 +87,25 @@ export default {
         this.countries = countries;
       });
     },
-    addCountry() {
-      this.country = {
-        name: '',
-        id: -1,
-      };
+    editCountry(country) {
       this.dialog = true;
     },
-    editCountry() {
-      this.dialog = true;
-    },
-    deleteCountry() {},
+    deleteCountry(country) {},
     save() {
-      API.addCountry(this.country).then((country) => {
-        this.country = country;
-        this.load();
-      });
+      API.addCountry(this.country)
+        .then((country) => {
+          this.load();
+        })
+        .catch((error) => alert(`error adding country. ${error}`));
 
-      this.dialog = false;
+      this.close();
     },
-    cancel() {
+    close() {
       this.dialog = false;
+      this.countryName = '';
     },
   },
 };
 </script>
 
-<style scoped>
-v-container {
-  background: red;
-}
-</style>
+<style scoped></style>
